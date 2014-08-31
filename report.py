058ad45bdf451be4305f8fcbd1ee01c41ef9f616
James Blachly <james.blachly@gmail.com>
2014-08-31 13:58:47 -0400

    Report.check_templates()

--- report.py
+++ report.py
@@ -3,11 +3,15 @@
 # create, format, and write an executive report
 # on the muCorrelated sample variants
 
+import os
 import pandas as pd
 
 class Report(object):
     '''Create, format, and write an executive report on the muCorrelated sample variants'''
-
+    
+    reporttypes = ['latex']    
+    templates = ['report.tex']
+    
     def __init__(self, study, variantsDataFrame):
         if study == '': raise ValueError('study name was an empty string')
         if not isinstance(variantsDataFrame, pd.DataFrame): raise TypeError('variantsDataFrame was not an instance of pandas.DataFrame')
@@ -16,6 +20,14 @@ class Report(object):
 
     def write(self, filename, filetype="latex"):
         if filename == '': raise ValueError('filename was an empty string')
-        if filetype not in ['latex']: return ValueError('filetype {} unrecognized'.format(filetype))
+        if filetype not in self.reporttypes: return ValueError('filetype {} unrecognized'.format(filetype))
         pass
+
+    @classmethod
+    def check_templates(cls):
+        '''Ensure the report templates are available. Raise exception if not.'''
+        dirname = os.path.dirname(os.path.abspath(__file__))
+        for templ in cls.templates:
+            if not os.path.isfile(os.path.join(dirname,templ)):
+                raise RuntimeError('Report template {} not found'.format(os.path.join(dirname,templ)))
 
