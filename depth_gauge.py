a0483162aab981ef5a5e8095b8123124e001ba6a
Karl Kroll <kwkroll.32@gmail.com>
2015-07-13 17:10:26 -0400

    modified depth gauge, adding features as indicated by the latest revision of the Vaggregate Supplemental Materials document. Includes, --point, --count, support for --regions on the command line (to trump any in the JSON), and minor bug fixes

--- depth_gauge.py
+++ depth_gauge.py
@@ -12,6 +12,8 @@ from __future__ import print_function
 # python standard modules
 import os
 import sys
+reload(sys)  
+sys.setdefaultencoding('utf8')
 import time
 import argparse
 import csv
@@ -55,6 +57,9 @@ def parseJSON(json_config):
 
     # write dictionary values into a more human-friendly config class
     config.outputDir = os.path.expanduser(JD['outputDir'])
+    
+    # no longer take region definition from json config file
+    #   command line argument now
     if str(JD['regions']):
         config.regions = JD['regions']
     else:
@@ -64,15 +69,55 @@ def parseJSON(json_config):
     config.samples = []
     config.bams = {}
     for i in JD['samples']:
-        for bam in i['bam']:
-            config.inputFiles.append(bam['path'])
-            config.samples.append(i['id'])
-            try:
-                config.bams[i['id']].append(bam['path'])
-            except:
-                config.bams[i['id']] = []
-                config.bams[i['id']].append(bam['path'])
+        try:
+            if len(i['bam']) >= 1:
+                for bam in i['bam']:
+                    config.inputFiles.append(bam['path'])
+                    config.samples.append(i['id'])
+                    try:
+                        config.bams[i['id']].append(bam['path'])
+                    except:
+                        config.bams[i['id']] = []
+                        config.bams[i['id']].append(bam['path'])
+        except:
+            throwWarning("Sample {0} has no BAM file!".format(i))
     return config 
+
+def parseAndValidateRegions(regions, json_config):
+    print("== Parsing and Validating Regions ==")
+    regions_list = []
+    if regions: # user has defined regions to focus on
+        for i in str(regions).split(','):
+            print("\t{0}".format(i))
+            if str(i.split('.')[-1]).lower() == "bed":
+                # this is a bed file of regions
+                # it will be parsed in the main python script
+                if os.path.isfile(str(i)):
+                    regions_list.append(os.path.expanduser(str(i)))
+                else:
+                    abortWithMessage("BED file {0} cannot be found".format(str(i)))
+            elif str(str(i).split(':')[0]).startswith('chr') and str(str(i).split(':')[0]) != "chr":
+                # this looks like a 'chromosome:start-stop' formatted region
+                try:                                    # does the input region have valid start and ends?
+                    int(str(str(i).split(':')[1])[0])
+                    int(str(str(i).split(':')[1])[1])
+                    regions_list.append(i)
+                except ValueError:                      # start and/or end are invalid
+                    abortWithMessage("Region {0} is not valid. Follow standard convention, Ex: chr1:100-300".format(str(i)))
+                except IndexError:                      # only chromosome was defined. this is permitted (whole chromosome region)
+                    regions_list.append(i)
+            else:
+                abortWithMessage("Region {0} is not a bed file or valid region.".format(i))
+        json_config.regions = regions_list
+    elif not regions and json_config.regions: 
+        # user declared no regions when running depth gauge, but there are usable regions in the json
+        for i in json_config.regions:
+            print("\t{0}".format(i))
+        return json_config
+    else:
+        # there are no regions to interrogate with depth gauge
+        abortWithMessage("Regions not set")
+    return json_config
 
 def parseRegionBed(regionfile, regionDictIn):
     ''' 
@@ -116,10 +161,12 @@ def GaugeDepth(config) :
             try:
                 reg_str = str(str(item.split(':')[1]).split('-')[0])
                 reg_end = str(str(item.split(':')[1]).split('-')[1])
+                name = str(reg_chr) + ":" + str(reg_str) + "-" + str(reg_end)
             except IndexError:                                  # represent whole chromosome regions [ex: chr2] by chrN:0-0 in the region dictionary   
-                reg_str = 0
-                reg_end = 0
-            regionDict[reg_chr].add((reg_str, reg_end))
+                reg_str = 1
+                reg_end = 1E9
+                name = str(reg_chr)
+            regionDict[reg_chr].add((reg_str, reg_end, name))
     if not regionDict:
         abortWithMessage("Regions not set!")
 
@@ -136,9 +183,13 @@ def GaugeDepth(config) :
                 continue
             for contig, ROI in regionDict.items():
                 for window in ROI:
-                    bed_name = window[2]
+                    try:
+                        bed_name = window[2]
+                    except:
+                        stop()
                     # make window 0
                     window = [int(window[0]) - 1, int(window[1])]
+                    
                     
                     # loop over all ROIs, checking this bam 
                     if config.p:
@@ -236,10 +287,12 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("json", help="Pass 1 json config as an argument")
-    parser.add_argument("-p", action="store_true", help="point-location coverage approximation on middle of region windows")
-    parser.add_argument("-c", action="store_true", help="report total number of reads in each region window (not an average)")
+    parser.add_argument("-p", "--point", dest='p', action="store_true", help="point-location coverage approximation on middle of region windows")
+    parser.add_argument("-c", "--count", dest='c', action="store_true", help="report total number of reads in each region window (not an average)")
+    parser.add_argument("-r", "--regions", default="", help="Comma separated list of bed regions and/or bed files by which to limit output. Ex: chr1:10230-10240,chr2,my_regions.bed")
     args = parser.parse_args()
     config = parseJSON(args.json)
+    config = parseAndValidateRegions(args.regions, config)
     config.p = bool(args.p)
     config.c = bool(args.c)
     if config.p and config.c:
