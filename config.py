dcb1d1bfb861a55c24774c102ede7bb4fe95b2d2
james.blachly@gmail.com <james.blachly@gmail.com>
2015-07-21 10:03:31 -0400

    GNU GPLv3 license text added

--- config.py
+++ config.py
@@ -1,7 +1,19 @@
-# config.py
-# 
-# James S Blachly MD
-# 2014-10-04
+#    Copyright 2013-2015 James S Blachly, MD and The Ohio State University
+#
+#    This file is part of Mucor.
+#
+#    Mucor is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Mucor is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
 class Config(object):
     """Object to hold configuration options passed from cmdline or parsed from JSON config file"""
